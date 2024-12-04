7c318f53755b0b3b54b74d987a027e9305132e46
Luciefur <luciefur6393473659@gmail.com>
2024-12-04 19:01:12 +0530

    cahnged jwt expiry in auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -4,82 +4,88 @@ import {} from "dotenv/config";
 import jwt from "jsonwebtoken";
 
 const signup = async (req, res) => {
-	try {
-		const { name, email, password, role } = req.body;
-		if (!name || !email || !password) {
-			return res
-				.status(400)
-				.json({ message: "Please provide all fields" });
-		}
+  try {
+    const { name, email, password, role } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please provide all fields" });
+    }
 
-		const user = await User.findOne({ email });
-		if (user) {
-			return res.status(400).json({ message: "User already exists" });
-		}
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-		const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-		const newUser = await User.create({
-			name,
-			email,
-			role,
-			password: hashedPassword,
-		});
+    const newUser = await User.create({
+      name,
+      email,
+      role,
+      password: hashedPassword,
+    });
 
-		const token = jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
-		res.cookie("token", token, {
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-			maxAge: 3 * 24 * 60 * 60 * 1000,
-		}).status(201).json({
-			_id: newUser._id,
-			name: newUser.name,
-			email: newUser.email,
-			role: newUser.role,
-		});
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+      })
+      .status(201)
+      .json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const login = async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		if (!email || !password) {
-			return res
-				.status(400)
-				.json({ message: "Please provide all fields" });
-		}
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please provide all fields" });
+    }
 
-		const user = await User.findOne({ email });
-		if (!user) {
-			return res.status(400).json({ message: "User not found" });
-		}
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
 
-		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-		if (!isPasswordCorrect) {
-			return res.status(400).json({ message: "Incorrect password" });
-		}
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Incorrect password" });
+    }
 
-		const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
 
-		res.cookie("token", token, {
-			httpOnly: true,
-			secure: process.env.NODE_ENV === "production",
-			sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-			maxAge: 3 * 24 * 60 * 60 * 1000,
-		}).status(200).json({
-			_id: user._id,
-			name: user.name,
-			email: user.email,
-			role: user.role,
-		});
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 3 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export { signup, login };
